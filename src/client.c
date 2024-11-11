a7307e29e0fc8e52c6fb3a686507ae7c4cb9a993
Your Name <you@example.com>
2024-11-11 00:12:05 -0500

    el gran cambio

--- src/client.c
+++ src/client.c
@@ -6,53 +6,162 @@
 /*   By: mabril <mabril@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 14:13:08 by mabril            #+#    #+#             */
-/*   Updated: 2024/11/07 20:37:14 by mabril           ###   ########.fr       */
+/*   Updated: 2024/11/11 00:07:15 by mabril           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-void ft_send_bit(int pid, char c)
+static int s_received = 0;
+
+void confirm_handle(int sig)
 {
-	int bit;
+	if(sig == SIGUSR1)
+		s_received = 1;
+}
+
+// void confirm_hendle(int sin)
+// {
 	
-	bit =0;
-	while(bit < 8)
+// 	void ft_handler(int sig)
+// 	{
+// 		static int bit;
+// 		static int char_bin;
+
+// 		// ft_printf("%d --", sig);
+// 		if (sig == SIGUSR1)
+// 		{
+// 			// ft_printf("bit %d = 1\n", bit);
+// 			char_bin |= (0x01 << bit);
+// 		}
+// 		// else
+// 		// 	ft_printf("bit %d = 0\n", bit);
+// 		bit++;
+// 		if (bit == 8)
+// 		{
+// 			ft_printf("%c", char_bin);
+// 			if(char_bin == '\n')
+// 			{
+// 				exit(1);
+				
+// 			}
+// 				bit = 0;
+// 				char_bin = 0;
+			
+// 		}
+// 	}
+// }
+
+void ft_send_bits_no_conf(int pid, unsigned int num, int bits)
+{
+    int bit;
+    
+    bit = 0;
+    while (bit < bits)
+    {
+        if ((num & (1 << bit)))
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        usleep(100);
+        bit++;
+    }
+}
+
+void	ft_send_bits(int s_pid, unsigned int num, int bits)
+{
+	int	i;
+	// int b;
+	
+	// b = 1;
+	i = 0;
+	while (i < bits)
 	{
-		
-		if((c & (1 << bit)) != 0)
-			kill(pid, SIGUSR1);
+		s_received = 0;
+		if ((num & (1 << i)))
+		{
+			// ft_printf("bit %d - %d = 1\n", i,b);
+			kill(s_pid, SIGUSR1);
+		}
 		else
-			kill(pid, SIGUSR2);
-		usleep(30);
-		bit++;	
+		{
+			kill(s_pid, SIGUSR2);
+			// ft_printf("bit %d - %d = 0\n", i,b);
+			
+		}
+		// b= b*2;
+		while(!s_received)
+			usleep(100);
+		i++;
 	}
 	// ft_printf(" c = %c\n", c);
 }
 
+void	ft_send_str(int s_pid, char *str, int len)
+{
+    int i;
+
+    i = 0;
+    while (i < len)
+    {
+        ft_send_bits(s_pid, str[i], 8);  // Enviamos 8 bits por carácter
+        i++;
+    }
+	ft_send_bits(s_pid, '\0', 8);
+}
+// void	ft_send_str(int pid, char *str, int len)
+// {
+// 	int	bit;
+// 	int b;
+// 	int i;
+
+// 	b =1;
+// 	bit = 0;
+// 	ft_printf(" c = %c\n", c);
+// 	while (bit < 8)
+// 	{
+// 		if ((c & (1 << bit)) != 0)
+// 		{
+// 			ft_printf("bit %d - %d = 1\n", bit,b);
+// 			kill(pid, SIGUSR1);
+// 		}
+// 		else
+// 		{
+// 			ft_printf("bit %d - %d = 0\n", bit,b);
+// 			kill(pid, SIGUSR2);
+// 		}
+// 		b= b*2;
+// 		usleep(100);
+// 		bit++;
+// 	}
+// 	ft_printf(" %c cararter enviado\n", c);
+// }
+
 int	main(int ac, char **av)
 {
-	int pid;
-	int i;
-	// int len;
+	int	server_pid;
+	// int	i;
+	int	len;
+	int client_pid;
 
-	i = 0;
-	if(ac == 3)
-	{
-		pid = ft_atoi(av[1]);
-		// len = ft_strlen(av[1]);
-		while (av[2][i])
-		{
-			ft_send_bit(pid, av[2][i]);
-			i++;
-		}	
-		ft_send_bit(pid, '\n');
-	}
-	else
+	// i = 0;
+	len = 0;
+	if (ac != 3)
 	{
 		ft_printf("\033[91mError: wrong format.\033[0m\n");
 		ft_printf("\033[33mTry: ./client <PID> <MESSAGE>\033[0m\n");
 		return (1);
 	}
+	signal(SIGUSR1, confirm_handle);
+	client_pid = getpid();
+	ft_printf("c_pid %d \n", client_pid);
+	server_pid = ft_atoi(av[1]);
+	len = ft_strlen(av[2]);
+	ft_printf("len %d \n", len);
+	ft_send_bits_no_conf(server_pid, client_pid, 32);
+	ft_send_bits(server_pid, len, 32);
+	ft_send_str(server_pid, av[2], len);
+	
+	// ft_printf(" len = %d\n", len);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,51 +6,208 @@
 /*   By: mabril <mabril@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 14:13:08 by mabril            #+#    #+#             */
-/*   Updated: 2024/11/07 20:26:49 by mabril           ###   ########.fr       */
+/*   Updated: 2024/11/11 00:07:31 by mabril           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-void ft_handler(int sig)
-{
-	static int bit;
-	static int char_bin;
+// static g_state = 0;
+static int g_state = 0;
 
+char	*new_str(int len)
+{
+	char	*str;
+
+	// ft_printf("entramos new_str\n");
+	str = ft_calloc((len + 1), sizeof(char));
+	// ft_printf("str = %s \n", str);
+	len = strlen(str);
+	// ft_printf("len str serv = %d \n", len);
+	
+	if (!str)
+	{
+		// ft_printf("la cadena no exite");
+		exit(1);
+	}
+		
+	return (str);
+}
+
+int	ft_client_pid(int sig)
+{
+	static int			bit;
+	static unsigned int	num;
+	int tem;
+	
+	tem =0;
+	// ft_printf("sig %d = ", sig);
 	if (sig == SIGUSR1)
-		char_bin |= (0x01 << bit);
+	{
+		// ft_printf("bit %d = 1\n", bit);
+		num |= (1u << bit);
+	}
+	// else
+		// ft_printf("bit %d = 0\n", bit);
+	bit++;
+	if (bit == 32)
+	{
+		// ft_printf("entra str \n");
+		// ft_printf("str en size = %s \n", str);
+		tem = num;
+		num = 0;
+		bit = 0;
+		g_state = 1;
+		return (tem);
+	}
+	return (num);
+}
+char	*ft_handler_size(int sig)
+{
+	static int			bit;
+	static unsigned int	num;
+	char				*str;
+
+	str = NULL;
+	// ft_printf("sig size = %d ", sig);
+	if (sig == SIGUSR1)
+	{
+		// ft_printf("bit %d = 1\n", bit);
+		num |= (1u << bit);
+	}
+	// else
+		// ft_printf("bit %d = 0\n", bit);
+	bit++;
+	if (bit == 32)
+	{
+		ft_printf("len = %d \n", num);
+
+		str = new_str(num);
+		ft_printf("str en size = %s \n", str);
+	 	g_state = 2;
+		bit = 0;
+		num = 0;
+		return (str);
+	}
+	return (str);
+}
+
+void	*ft_handler_char(int sig, char *str)
+{
+	static int	bit;
+	static int	c;
+	static int	i;
+
+	// ft_printf("str en hand char = %s \n", str);
+	// ft_printf("sig = %d ", sig);
+	// ft_printf("bit = %d ", bit);
+	if (sig == SIGUSR1)
+	{
+		ft_printf("bit %d = 1\n", bit);
+		c |= (0x01 << bit);
+	}
+	else
+		ft_printf("bit %d = 0\n", bit);
 	bit++;
 	if (bit == 8)
 	{
-		ft_printf("%c", char_bin);
-		// if(char_bin == '\n')
-		// 	exit(1);
+		str[i] = c;
+		ft_printf("str[i] = %c \n", str[i]);
+		if (c == '\0')
+		{
+			
+			ft_printf("estamos entrano b- 0");
+			ft_printf("%s", str);
+			free(str);
+			str = NULL;
+		 	g_state = 0;
+			i = 0;
+		}
 		bit = 0;
-		char_bin = 0;
+		c = 0;
 	}
+	return (str);
 }
+
+// void ft_handler(int sig)
+// {
+// 	static int bit;
+// 	static int char_bin;
+
+// 	// ft_printf("%d --", sig);
+// 	if (sig == SIGUSR1)
+// 	{
+// 		// ft_printf("bit %d = 1\n", bit);
+// 		char_bin |= (0x01 << bit);
+// 	}
+// 	// else
+// 	// 	ft_printf("bit %d = 0\n", bit);
+// 	bit++;
+// 	if (bit == 8)
+// 	{
+// 		ft_printf("%c", char_bin);
+// 		if(char_bin == '\n')
+// 		{
+// 			exit(1);
+			
+// 		}
+// 			bit = 0;
+// 			char_bin = 0;
+		
+// 	}
+// }
+
+void	ft_handler(int sig)
+{
+	static char	*str;
+	static int c_pid;
+	// int len=0;
+	
+	
+	
+	if  (g_state == 0)
+	{
+		c_pid = ft_client_pid(sig);
+		
+		// len = ft_strlen(str);
+		ft_printf("c_pid = %d  \n", c_pid);
+		// ft_printf("len hande = %d  \n", len);
+	}
+	else if  (g_state == 1)
+	{
+		str = ft_handler_size(sig);
+		// ft_printf("c_pid = %s  \n", str);
+		
+		// len = ft_strlen(str);
+		// ft_printf("str en hand = %s  \n", str);
+		// ft_printf("len hande = %d  \n", len);
+	}
+	else if (g_state == 2)
+	{
+		// ft_printf("entramos para hacer poner los c\n");
+
+		str = ft_handler_char(sig, str);
+	}
+	if (c_pid)
+        kill(c_pid, SIGUSR1); 
+}
+
 int	main(int ac, char **av)
 {
-	int pid;
-	
-	if(ac != 1 && av)
+	int	pid;
+
+	if (ac != 1 && av)
 	{
 		ft_printf(RED "error\n" RESET);
 		ft_printf(YELLOW "Try: ./server\n" RESET);
 	}
-	
-	
-		pid = getpid();
-		ft_printf( BLUE "%s" CYAN "%s" RESET "%d\n", "PID",  " -> ", pid);
-		signal(SIGUSR1, ft_handler);
-		signal(SIGUSR2, ft_handler);
-		while (1)
-		{
-			pause();
-		}	
-	
+	pid = getpid();
+	ft_printf(BLUE "%s" CYAN "%s" RESET "%d\n", "PID", " -> ", pid);
+	signal(SIGUSR1, ft_handler);
+	signal(SIGUSR2, ft_handler);
+	while (1)
+		pause();
 	return (0);
 }
 
-
-// addssdsfsdfsdfsfdsdsfdsfdsfsfsdfsdfsdfdsfdsfsdfdsfsfsdfdsfsfdsssdfsdfsdfdsfsdsdtequieroasdfsdffasdfasdfasdfasdfasdfasdfasdfdsafasdfasdfasdfasdfasdfsadfsadfasfasdfasdfasdfasdfasdfasdfasdfasdfsdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfasdfaasdfssdfdsfsfsdffsdsfsdffsfdfsdsfdsdfsdsfsfdsfsdfsdfsdffsfjsdhfksjdhjkfshkfhsdfhksdfhdskjfhsjkfhsjkfhsdjkfhdjkhfjkdshfjksdhfjdksfhdjkshfkdjshfjkdhskjfhdskjfhkjsdhfjkshkjdhfjkshfkjshfkjsdhfjsdhfdksdhjkhfkjsdhfjkdshfjshdfkjdshfkjdshfjkdshfjksdhfkjdsfhsdjkfhdsjkfhuwehriuhriuwehiuewhuiheuifheiuhferuygheurhfeuifhdsuifhfjaksjdfjksagfhkeuyfghjbdkaefwygfiekywgfhjdsabfhjcbbhruewhruewbckfjbdjkhakufgdasufhkudcbkyasbuehuyegwuifhejkacgsfkjagjdscgfuyfwiuygwqeoegieygyrglkuewrowqgeyobweyucreqwgrcoborewbugruiebcrgwqobecruigewbioructiuweryoceiruwcbowequbcoiuwebcroybcuiwqeyriucbhdfkfjefhiuheiufwehiruewhriuyqruiwyrxonuyeriuonqerxgerofguihnuiehwiuhdiuewhddisuhflusdkhafuihkjsldajfljdsakjflkjasdlkfjsakldjfklsdjfklsdjfdlksjfdkslafjklsdjfklsjfsakljfdklsajlfjioewjriowrhqweiorhiudfhiudayfduiashufidafhouiqhweiurghufdshaflashiuqguiyegfoiuhsdoafhiugdyudshfuisdahuisadhfuiahfiufbyeubcuffsdfadsfasdfasdfasdfsdfasdfsdafasdfadshjkasdfhgjsadfgksajdgfhjsgdhjfgsahjdfghjsagjhfkashdfgkasghdjfhgksadgfjhshagdfdsafasdfsdfsdfdsfdsfdsfsdafdfdsfsdfsfdsfdsfsdffewrwerwertettyrtyrtyrtyrtyryrtytrytreryeyttttttttttttttttertyertyertyertyertytryrtryeyyyyyysalsa
+// addssdsfsdfsdfsfdsdsfdsfdsfsfsdfsdfsdfdsfdsfsdfdsfsfsdfdsfsfdsssdfsdfsdfdsfsdsdtequieroasdfsdffasdfasdfasdfasdfasdfasdfasdfdsafa sdfasdfasdfasdfasdfsadfsadfasfasdfasdfasdfasdfasdfasdfasdfasdfsdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfasdfaasdfssdfdsfsfsdffsdsfsdffsfdfsdsfdsdfsdsfsfdsfsdfsdfsdffsfjsdhfksjdhjkfshkfhsdfhksdfhdskjfhsjkfhsjkfhsdjkfhdjkhfjkdshfjksdhfjdksfhdjkshfkdjshfjkdhskjfhdskjfhkjsdhfjkshkjdhfjkshfkjshfkjsdhfjsdhfdksdhjkhfkjsdhfjkdshfjshdfkjdshfkjdshfjkdshfjksdhfkjdsfhsdjkfhdsjkfhuwehriuhriuwehiuewhuiheuifheiuhferuygheurhfeuifhdsuifhfjaksjdfjksagfhkeuyfghjbdkaefwygfiekywgfhjdsabfhjcbbhruewhruewbckfjbdjkhakufgdasufhkudcbkyasbuehuyegwuifhejkacgsfkjagjdscgfuyfwiuygwqeoegieygyrglkuewrowqgeyobweyucreqwgrcoborewbugruiebcrgwqobecruigewbioructiuweryoceiruwcbowequbcoiuwebcroybcuiwqeyriucbhdfkfjefhiuheiufwehiruewhriuyqruiwyrxonuyeriuonqerxgerofguihnuiehwiuhdiuewhddisuhflusdkhafuihkjsldajfljdsakjflkjasdlkfjsakldjfklsdjfklsdjfdlksjfdkslafjklsdjfklsjfsakljfdklsajlfjioewjriowrhqweiorhiudfhiudayfduiashufidafhouiqhweiurghufdshaflashiuqguiyegfoiuhsdoafhiugdyudshfuisdahuisadhfuiahfiufbyeubcuffsdfadsfasdfasdfasdfsdfasdfsdafasdfadshjkasdfhgjsadfgksajdgfhjsgdhjfgsahjdfghjsagjhfkashdfgkasghdjfhgksadgfjhshagdfdsafasdfsdfsdfdsfdsfdsfsdafdfdsfsdfsfdsfdsfsdffewrwerwertettyrtyrtyrtyrtyryrtytrytreryeyttttttttttttttttertyertyertyertyertytryrtryeyyyyyysalsa
